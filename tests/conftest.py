b55474868073c776057ba62923cb3915a4eb107b
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-25 23:04:22 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,17 @@
+import os
+
 from hypothesis import (HealthCheck,
+                        Verbosity,
                         settings)
 
+on_azure_pipelines = bool(os.getenv('TF_BUILD', False))
+on_travis_ci = bool(os.getenv('CI', False))
 settings.register_profile('default',
+                          max_examples=(settings.default.max_examples // 5
+                                        if on_azure_pipelines or on_travis_ci
+                                        else settings.default.max_examples),
                           deadline=None,
                           suppress_health_check=[HealthCheck.filter_too_much,
-                                                 HealthCheck.too_slow])
+                                                 HealthCheck.too_slow],
+                          verbosity=Verbosity(settings.default.verbosity
+                                              + on_travis_ci))
