089d8455ed7e2b54638031371eab23764758ae3e
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-29 22:49:21 +0300

    Add docstring for 'constrained_delaunay_triangles' function

--- sect/triangulation.py
+++ sect/triangulation.py
@@ -81,6 +81,31 @@ def constrained_delaunay_triangles(border: Contour,
                                    extra_points: Sequence[Point] = (),
                                    extra_constraints: Sequence[Segment] = ()
                                    ) -> List[Triangle]:
+    """
+    Returns subdivision of the polygon given by border and holes
+    (with potentially extra points and constraints)
+    into triangles.
+
+    :param border: border of the polygon.
+    :param holes: holes of the polygon.
+    :param extra_points:
+        additional points to be presented in the triangulation.
+    :param extra_constraints:
+        additional constraints to be presented in the triangulation.
+    :returns: subdivision of the border, holes & extra points
+    considering constraints into triangles.
+
+    >>> (constrained_delaunay_triangles([(0, 0), (3, 0), (0, 3)])
+    ...  == [((0, 0), (3, 0), (0, 3))])
+    True
+    >>> (constrained_delaunay_triangles([(0, 0), (3, 0), (3, 3), (0, 3)])
+    ...  == [((0, 3), (3, 0), (3, 3)), ((0, 0), (3, 0), (0, 3))])
+    True
+    >>> (constrained_delaunay_triangles([(0, 0), (3, 0), (1, 1), (0, 3)])
+    ...  == [((0, 0), (3, 0), (1, 1)),
+    ...      ((0, 0), (1, 1), (0, 3))])
+    True
+    """
     return (constrained_delaunay(border, holes,
                                  extra_points=extra_points,
                                  extra_constraints=extra_constraints)
