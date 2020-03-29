c6095abf38499e0a34623d44d9a0d3b609efcf3d
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-29 23:02:52 +0300

    Add docs

---
+++ docs/conf.py
@@ -0,0 +1,56 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+from datetime import date
+import sect
+
+project = sect.__name__
+copyright = '{}, Azat Ibrakov'.format(date.today().year)
+author = 'Azat Ibrakov'
+
+# The full version, including alpha/beta/rc tags
+release = sect.__version__
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The master toctree document.
+master_doc = 'index'
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+autodoc_member_order = 'bysource'
---
+++ docs/index.rst
@@ -0,0 +1,10 @@
+Welcome to sect's documentation!
+================================
+
+.. note::
+   If object is not listed in documentation
+   it should be considered as implementation detail
+   that can change and should not be relied upon.
+
+.. automodule:: sect.triangulation
+    :members:
