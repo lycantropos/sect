e64dcf53803e6726d970fe18331f664d33303c68
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-29 08:18:15 +0300

    Complete test for 'constrained_delaunay_triangles' function

--- tests/triangulation_tests/test_constrained_delaunay_triangles.py
+++ tests/triangulation_tests/test_constrained_delaunay_triangles.py
@@ -4,6 +4,7 @@ from hypothesis_geometry.hints import Polygon
 from sect.core.utils import (contour_to_segments,
                              flatten)
 from sect.triangulation import constrained_delaunay_triangles
+from tests import to_boundary_endpoints
 from . import strategies
 
 
@@ -46,3 +47,14 @@ def test_edges(polygon: Polygon) -> None:
     assert (set(flatten(map(contour_to_segments, result)))
             >= set(sum(map(contour_to_segments, holes),
                        contour_to_segments(border))))
+
+
+@given(strategies.polygons)
+def test_boundary(polygon: Polygon) -> None:
+    border, holes = polygon
+
+    result = constrained_delaunay_triangles(border, holes)
+
+    assert (to_boundary_endpoints(result)
+            == set(map(frozenset, sum(map(contour_to_segments, holes),
+                                      contour_to_segments(border)))))
