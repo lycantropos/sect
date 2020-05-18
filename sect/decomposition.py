ee2087e39f0bc283c3c8b380837b24b4fb817c71
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-18 15:02:07 +0300

    Add docstring for 'trapezoidal_polygon' function

--- sect/decomposition.py
+++ sect/decomposition.py
@@ -14,6 +14,44 @@ Location = Location
 def trapezoidal_polygon(border: Contour, holes: Sequence[Contour] = (),
                         *,
                         shuffler: Shuffler = random.shuffle) -> Map:
+    """
+    Returns trapezoidal map of the polygon.
+
+    Based on incremental randomized algorithm by R. Seidel.
+
+    Time complexity:
+        ``O(vertices_count * log vertices_count)`` expected,
+        ``O(vertices_count ** 2)`` worst,
+        where ``vertices_count = len(border) + sum(map(len, holes))``.
+    Memory complexity:
+        ``O(vertices_count)``,
+        where ``vertices_count = len(border) + sum(map(len, holes))``.
+    Reference:
+        https://doi.org/10.1016%2F0925-7721%2891%2990012-4
+        https://www.cs.princeton.edu/courses/archive/fall05/cos528/handouts/A%20Simple%20and%20fast.pdf
+
+    :param border: border of the polygon.
+    :param holes: holes of the polygon.
+    :param shuffler:
+        function which mutates sequence by shuffling its elements,
+        required for randomization.
+    :returns: trapezoidal map of polygon.
+
+    >>> map_ = trapezoidal_polygon([(0, 0), (6, 0), (6, 6), (0, 6)],
+    ...                            [[(2, 2), (2, 4), (4, 4), (4, 2)]])
+    >>> (1, 1) in map_
+    True
+    >>> (2, 2) in map_
+    True
+    >>> (3, 3) in map_
+    False
+    >>> map_.locate((1, 1)) is Location.INTERIOR
+    True
+    >>> map_.locate((2, 2)) is Location.BOUNDARY
+    True
+    >>> map_.locate((3, 3)) is Location.EXTERIOR
+    True
+    """
     return Map.from_polygon(border, holes, shuffler)
 
 
