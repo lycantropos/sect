afb4f5cdd35bd562164d0ec8a2a0a1f5c719e175
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-18 03:43:43 +0300

    Complete imports

--- sect/decomposition.py
+++ sect/decomposition.py
@@ -1,11 +1,13 @@
 import random
 
+from .core.location import Location
 from .core.node import Node
 from .core.trapezoidal_map import build_graph as _build_graph
 from .hints import (Contour,
                     Shuffler)
 
 Node = Node
+Location = Location
 
 
 def trapezoidal(contour: Contour,
