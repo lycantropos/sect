5669de14f2e651b0666a1dd278cab017236eab18
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-19 23:49:17 +0700

    Add docstrings for 'Event' class

--- sect/core/event.py
+++ sect/core/event.py
@@ -47,31 +47,87 @@ class Event:
 
     @property
     def end(self) -> Point:
+        """
+        Returns end of the event's segment.
+
+        >>> event = Event(True, (0, 0), None, False, EdgeKind.NORMAL)
+        >>> event.complement = Event(False, (1, 0), event, False,
+        ...                          EdgeKind.NORMAL)
+        >>> event.end == (1, 0)
+        True
+        """
         return self.complement.start
 
     @property
     def is_vertical(self) -> bool:
+        """
+        Checks whether the event's segment aligned with y-axis.
+
+        >>> event = Event(True, (0, 0), None, False, EdgeKind.NORMAL)
+        >>> event.complement = Event(False, (0, 1), event, False,
+        ...                          EdgeKind.NORMAL)
+        >>> event.is_vertical
+        True
+        """
         start_x, _ = self.start
         end_x, _ = self.end
         return start_x == end_x
 
     @property
     def is_horizontal(self) -> bool:
+        """
+        Checks whether the event's segment aligned with x-axis.
+
+        >>> event = Event(True, (0, 0), None, False, EdgeKind.NORMAL)
+        >>> event.complement = Event(False, (1, 0), event, False,
+        ...                          EdgeKind.NORMAL)
+        >>> event.is_horizontal
+        True
+        """
         _, start_y = self.start
         _, end_y = self.end
         return start_y == end_y
 
     @property
     def in_intersection(self) -> bool:
+        """
+        Checks whether the event's segment belongs to intersection.
+
+        >>> event = Event(True, (0, 0), None, False, EdgeKind.NORMAL)
+        >>> event.complement = Event(False, (1, 0), event, False,
+        ...                          EdgeKind.NORMAL)
+        >>> event.in_intersection
+        True
+        """
         edge_kind = self.edge_kind
         return (edge_kind is EdgeKind.NORMAL and not self.other_in_out
                 or edge_kind is EdgeKind.SAME_TRANSITION)
 
     @property
     def segment(self) -> Segment:
+        """
+        Returns segment of the event.
+
+        >>> event = Event(True, (0, 0), None, False, EdgeKind.NORMAL)
+        >>> event.complement = Event(False, (1, 0), event, False,
+        ...                          EdgeKind.NORMAL)
+        >>> event.segment == ((0, 0), (1, 0))
+        True
+        """
         return self.start, self.end
 
     def y_at(self, x: Coordinate) -> Coordinate:
+        """
+        Returns y-coordinate of the segment point by the x-coordinate.
+
+        >>> event = Event(True, (0, 0), None, False, EdgeKind.NORMAL)
+        >>> event.complement = Event(False, (1, 1), event, False,
+        ...                          EdgeKind.NORMAL)
+        >>> event.y_at(0) == 0
+        True
+        >>> event.y_at(1) == 1
+        True
+        """
         if self.is_vertical or self.is_horizontal:
             _, start_y = self.start
             return start_y
