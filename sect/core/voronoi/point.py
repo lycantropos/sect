cb3fb86eb978ec7ccb38f3398c972670ce043eeb
Azat Ibrakov <azatibrakov@gmail.com>
2020-11-24 13:12:40 +0300

    Complete 'are_vertical_endpoints' function

--- sect/core/voronoi/point.py
+++ sect/core/voronoi/point.py
@@ -26,4 +26,6 @@ class Point:
 
 
 def are_vertical_endpoints(start: Point, end: Point) -> bool:
-    return start.x == end.x
+    start_x, _ = start
+    end_x, _ = end
+    return start_x == end_x
