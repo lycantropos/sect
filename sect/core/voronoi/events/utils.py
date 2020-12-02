992707941c61908b39ab7caa7ccea8dfc5623144
Azat Ibrakov <azatibrakov@gmail.com>
2020-12-02 16:15:42 +0300

    Complete 'robust_sum_of_products_with_sqrt_triplets' function

--- sect/core/voronoi/events/utils.py
+++ sect/core/voronoi/events/utils.py
@@ -29,19 +29,19 @@ def robust_sum_of_products_with_sqrt_triplets(
         right: Tuple[Coordinate, Coordinate, Coordinate]) -> Coordinate:
     first_left, second_left, third_left = left
     first_right, second_right, third_right = right
-    a = robust_sum_of_products_with_sqrt_pairs((first_left, second_left),
-                                               (first_right, second_right))
-    b = robust_product_with_sqrt(third_left, third_right)
-    return (a + b
-            if a >= 0 and b >= 0 or a <= 0 and b <= 0
+    lh = robust_sum_of_products_with_sqrt_pairs((first_left, second_left),
+                                                (first_right, second_right))
+    rh = third_left * robust_sqrt(third_right)
+    return (lh + rh
+            if lh >= 0 and rh >= 0 or lh <= 0 and rh <= 0
             else
-            robust_sum_of_products_with_sqrt_pairs(
+            robust_divide(robust_sum_of_products_with_sqrt_pairs(
                     (first_left * first_left * first_right
                      + second_left * second_left * second_right
                      - third_left * third_left * third_right,
                      2 * first_left * second_left),
-                    (1, first_right * second_right))
-            / (a - b))
+                    (1, first_right * second_right)),
+                    lh - rh))
 
 
 def to_first_point_segment_segment_quadruplets_expression(
