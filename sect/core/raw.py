7731e2a86a425b0b8063bed30b1dce9c6cad2694
Azat Ibrakov <azatibrakov@gmail.com>
2021-02-01 21:45:58 +0300

    Complete 'raw' module: remove redundant functions

--- sect/core/raw.py
+++ sect/core/raw.py
@@ -1,17 +1,5 @@
-from ground.hints import Multipoint, Multipolygon, Multisegment, Polygon
+from ground.hints import Multisegment
 
 
 def from_multisegment(multisegment: Multisegment):
     return multisegment.segments
-
-
-def from_polygon(polygon: Polygon):
-    return polygon.border, polygon.holes
-
-
-def from_multipolygon(multipolygon: Multipolygon):
-    return [from_polygon(polygon) for polygon in multipolygon.polygons]
-
-
-def from_multipoint(multipoint: Multipoint):
-    return multipoint.points
