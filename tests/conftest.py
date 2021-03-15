7d0f61a07f8ebf99dca028dad33d89c80a3a82a7
Azat Ibrakov <azatibrakov@gmail.com>
2021-03-15 22:41:54 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,16 @@
 import os
-from decimal import getcontext
+import platform
 
-import pytest
 from hypothesis import (HealthCheck,
                         settings)
 
-from tests.utils import MAX_COORDINATE_EXPONENT
-
 on_azure_pipelines = bool(os.getenv('TF_BUILD', False))
+is_pypy = platform.python_implementation() == 'PyPy'
 settings.register_profile('default',
-                          max_examples=(settings.default.max_examples // 5
+                          max_examples=(settings.default.max_examples
+                                        // 5 * (1 + is_pypy)
                                         if on_azure_pipelines
                                         else settings.default.max_examples),
                           deadline=None,
                           suppress_health_check=[HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow])
-
-
-@pytest.fixture(autouse=True,
-                scope='session')
-def set_decimal_context() -> None:
-    getcontext().prec = MAX_COORDINATE_EXPONENT * 5
