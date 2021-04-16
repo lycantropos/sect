af3811f92e4008cf2d40f4bbb3681ed1c2247fca
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-16 14:13:14 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ on_azure_pipelines = bool(os.getenv('TF_BUILD', False))
 is_pypy = platform.python_implementation() == 'PyPy'
 settings.register_profile('default',
                           max_examples=(settings.default.max_examples
-                                        // 2 * (1 + 9 * is_pypy)
+                                        // 2 * (1 + 24 * is_pypy)
                                         if on_azure_pipelines
                                         else settings.default.max_examples),
                           deadline=None,
