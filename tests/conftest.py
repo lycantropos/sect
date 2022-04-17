f0f244e4a438b8ae3ccca0d8e4a5fccc65e8f355
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-18 01:47:49 +0700

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,27 @@
 import os
 import platform
+from datetime import timedelta
 
+import pytest
 from hypothesis import (HealthCheck,
                         settings)
 
-on_azure_pipelines = bool(os.getenv('TF_BUILD', False))
 is_pypy = platform.python_implementation() == 'PyPy'
+on_ci = bool(os.getenv('CI', False))
+max_examples = ((-settings.default.max_examples
+                 // (10 if is_pypy else 2))
+                if on_ci
+                else settings.default.max_examples)
 settings.register_profile('default',
-                          max_examples=(settings.default.max_examples
-                                        // (2 * (1 + 4 * is_pypy))
-                                        if on_azure_pipelines
-                                        else settings.default.max_examples),
-                          deadline=None,
-                          suppress_health_check=[HealthCheck.filter_too_much,
-                                                 HealthCheck.too_slow])
+                          deadline=(timedelta(hours=1) / max_examples
+                                    if on_ci
+                                    else None),
+                          max_examples=max_examples,
+                          suppress_health_check=[HealthCheck.too_slow])
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_sessionfinish(session: pytest.Session,
+                         exitstatus: pytest.ExitCode) -> None:
+    if exitstatus == pytest.ExitCode.NO_TESTS_COLLECTED:
+        session.exitstatus = pytest.ExitCode.OK
