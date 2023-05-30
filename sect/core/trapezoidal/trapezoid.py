130bea432c806f3352bac776e18aac954307b100
Azat Ibrakov <azatibrakov@gmail.com>
2023-05-30 09:14:04 +0200

    Complete order of 'Trapezoid' class members

--- sect/core/trapezoidal/trapezoid.py
+++ sect/core/trapezoidal/trapezoid.py
@@ -12,6 +12,53 @@ if TYPE_CHECKING:
 
 
 class Trapezoid:
+    @property
+    def component(self) -> bool:
+        """
+        Checks if the trapezoid is a component of decomposed geometry.
+        """
+        return self.below.interior_to_left and not self.above.interior_to_left
+
+    @property
+    def lower_left(self) -> Optional[Trapezoid]:
+        return self._lower_left
+
+    @lower_left.setter
+    def lower_left(self, value: Optional[Trapezoid]) -> None:
+        self._lower_left = value
+        if value is not None:
+            value._lower_right = self
+
+    @property
+    def lower_right(self) -> Optional[Trapezoid]:
+        return self._lower_right
+
+    @lower_right.setter
+    def lower_right(self, value: Optional[Trapezoid]) -> None:
+        self._lower_right = value
+        if value is not None:
+            value._lower_left = self
+
+    @property
+    def upper_left(self) -> Optional[Trapezoid]:
+        return self._upper_left
+
+    @upper_left.setter
+    def upper_left(self, value: Optional[Trapezoid]) -> None:
+        self._upper_left = value
+        if value is not None:
+            value._upper_right = self
+
+    @property
+    def upper_right(self) -> Optional[Trapezoid]:
+        return self._upper_right
+
+    @upper_right.setter
+    def upper_right(self, value: Optional[Trapezoid]) -> None:
+        self._upper_right = value
+        if value is not None:
+            value._upper_left = self
+
     __slots__ = ('left', 'right', 'below', 'above', 'node', '_lower_left',
                  '_lower_right', '_upper_left', '_upper_right')
 
@@ -31,50 +78,3 @@ class Trapezoid:
         self._upper_right: Optional[Trapezoid] = None
 
     __repr__ = generate_repr(__init__)
-
-    @property
-    def component(self) -> bool:
-        """
-        Checks if the trapezoid is a component of decomposed geometry.
-        """
-        return self.below.interior_to_left and not self.above.interior_to_left
-
-    @property
-    def lower_left(self) -> Optional[Trapezoid]:
-        return self._lower_left
-
-    @property
-    def lower_right(self) -> Optional[Trapezoid]:
-        return self._lower_right
-
-    @property
-    def upper_left(self) -> Optional[Trapezoid]:
-        return self._upper_left
-
-    @property
-    def upper_right(self) -> Optional[Trapezoid]:
-        return self._upper_right
-
-    @lower_left.setter
-    def lower_left(self, value: Optional[Trapezoid]) -> None:
-        self._lower_left = value
-        if value is not None:
-            value._lower_right = self
-
-    @lower_right.setter
-    def lower_right(self, value: Optional[Trapezoid]) -> None:
-        self._lower_right = value
-        if value is not None:
-            value._lower_left = self
-
-    @upper_left.setter
-    def upper_left(self, value: Optional[Trapezoid]) -> None:
-        self._upper_left = value
-        if value is not None:
-            value._upper_right = self
-
-    @upper_right.setter
-    def upper_right(self, value: Optional[Trapezoid]) -> None:
-        self._upper_right = value
-        if value is not None:
-            value._upper_left = self
